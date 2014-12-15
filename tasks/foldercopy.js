b09c60fcb818e89b251d6a311825fa563f169814
davewcampbell <dave.campbell@thecanadianpress.com>
2014-12-15 16:49:14 -0500

    added recursive pattern to copy, still missing copy to multiple destinations

--- tasks/foldercopy.js
+++ tasks/foldercopy.js
@@ -3,31 +3,79 @@
 var fs = require("fs-extra");
 var _ = require("lodash");
 var moment = require("moment");
+var winston = require("winston");
+var logger = require('../logger');
+var pathHelper = require('path');
 
-function copy(path, destinations, options){	
+function copy(parent, destination, options){	
 
-	var limit = (options != null) ? options.limit : null;
-	
-	var copy_callback = function(err, files){
-		_.forEach(files, function(file){
+	var extensions = (options)? options.extensions : null;
+	var limit = (options)? options.limit : null;
+	var recursive = (options)? options.recursive : null;
+	var preserveDirectoryStructure = (options)? options.preserveDirectoryStructure : null;
 
-			var source = path + file;
+	var copy_callback = function(err, paths){
 
-			_.forEach(destinations, function(destination){
-				var target = destination + file;
+		_.forEach(paths, function(uri){
 
-				fs.stat(source, function(err, stats){
-					if(err)	continue;
-					if(!limit || (limit && moment(stats.mtime).isBefore(limit))){
-						fs.copySync(source, target);
+			var source = pathHelper.resolve(parent, uri);			
+
+			fs.stat(source, function(err, stats){
+
+				if(err)	return;
+
+				if(stats.isDirectory() && recursive){
+
+					var target = (preserveDirectoryStructure)? pathHelper.resolve(destination,  uri) : destination;
+
+					if(!fs.existsSync(destination)){												
+						fs.mkdirSync(destination);
 					}
 
-				});		
-			});			
+					copy(source, target, options);
+				}
+
+				if(	stats.isFile() && 
+					isValidLimit(stats.mtime, limit) && 
+					isTargetExtension(extensions, pathHelper.extname(source))
+				){
+
+					if(!fs.existsSync(destination)){
+						fs.mkdirSync(destination);
+					}
+
+
+					var target = pathHelper.resolve(destination,  uri);
+					logger.log("Moving [" + source + "] to [" + target + "]");
+					fs.copySync(source, target);
+				}
+			});		
+						
 		});
 	};
 
-	fs.readdir(path, copy_callback);
+	fs.readdir(parent, move_callback);
 }
 
-module.exports.copy = copy;
+/*
+* returns true if the array supplied is null or empty, or if the element is contained in the array. 
+* Otherwise, false is returned
+*/
+function isTargetExtension(extensions, ext){
+	if(extensions == null || extensions.length == 0)
+		return true;
+
+	return _.contains(extensions, ext);
+}
+
+function isValidLimit(mtime, limit){
+	return (!limit || (limit && moment(mtime).isAfter(limit)));
+}
+
+function log(message){
+	winston.log(message);
+	winston.info(message);
+}
+
+module.exports.move = move;
+module.exports.setLogPath = function(path){logger.setLogPath(path);};
