57d5160b0442d91eeda65b066af8faf8756fbb22
davewcampbell <dave.campbell@thecanadianpress.com>
2014-12-18 14:04:42 -0500

    fixed up routes, added multipart support

--- app.js
+++ app.js
@@ -2,26 +2,51 @@
 
 
 var express = require('express');
+var multer = require('multer'); 
 var app = express();
 var filesRoute = require('./routes/files');
+var router = express.Router();
 
-var server = app.listen(3000, function () {
+// middleware to run for every request, just console logging
+router.use(function(req, res, next) {
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
+	// log each request to the console
+	console.log(req.method, req.url);
+	// continue doing what we were doing and go to the route
+	next();	
 });
 
+// define the uploads directory
+app.use('/file', multer({ 
+	dest: './uploads/',
+	rename: function (fieldname, filename) {
+    	return filename.replace(/\W+/g, '-').toLowerCase() + "-" + Date.now();
+  }
+}));
+
+
+// get file by id
+router.get('/file/:id?', filesRoute.getById);
+// save file with attachment
+router.post('/file', filesRoute.save);
+
+
 // Default
-app.get("/", function(req, res){
-	console.log('Received - Get on default route.')
+router.get("/", function(req, res){
 	res.status(200).send("<h1>Hello dorin</h1>");
 });
 
-// get file by id
-app.get('/file/:id?', filesRoute.getById);
-app.post('/file', filesRoute.save);
+//appy our routes
+app.use('/', router);
+
+// Start up the server
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
 
 module.exports = app;
