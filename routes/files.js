d9483426622acc50aaca7ee7164506f6d4b1462e
davewcampbell <dave.campbell@thecanadianpress.com>
2014-12-18 16:03:00 -0500

    Return url of asset on save. Implemented get by id functionality.

--- routes/files.js
+++ routes/files.js
@@ -1,11 +1,17 @@
 "use strict";
 
+var _ = require("lodash");
+var path = require("path");
+
 function save(request, response){
-	response.status(201).end();
+
+	var savedUrl = request.protocol + '://' + request.get('host') + "/file/" + request.files.file.name;
+	response.status(201).end(savedUrl);
 }
 
 function getById(request, response){
-	response.end("<h1>Not Found</h1>");
+	
+	response.sendFile(request.params.id, { root: path.join(__dirname, '../uploads') });
 }
 
 module.exports.save = save;
