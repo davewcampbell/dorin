6cd179ea416dd24f3f19038d0185be97d24967f9
davewcampbell <dave.campbell@thecanadianpress.com>
2014-12-19 16:21:45 -0500

    Moved the routes folder to the www folder as it is web specific.

---
+++ www/routes/files.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var _ = require("lodash");
+var path = require("path");
+
+function save(request, response){
+
+	var savedUrl = request.protocol + '://' + request.get('host') + "/file/" + request.files.file.name;
+	response.status(201).end(savedUrl);
+}
+
+function getById(request, response){
+	
+	response.sendFile(request.params.id, { root: path.join(__dirname, '../uploads') });
+}
+
+module.exports.save = save;
+module.exports.getById = getById;
