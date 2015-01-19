7bfc76b7dc27ec5e2aaf88073abdf0cd0215945c
Dave Campbell <davewcampbell@gmail.com>
2015-01-19 00:04:57 -0500

    added http calls to jobs api end points and assign to local variables to populate the edit and default views

--- www/lib/app/purge/controllers/purgeController.js
+++ www/lib/app/purge/controllers/purgeController.js
@@ -6,14 +6,34 @@
 		.controller("purgeController", purgeController);
 
 
-	function purgeController(){
+	function purgeController($http, $routeParams){
 		var vm = this;
 		vm.title = "Dorin";
 		vm.submit = submit;
+		vm.jobs = [];
+		vm.job = {};
 
 
 		function submit(){
 			console.log("submitted");
+		}
+
+		if($routeParams.id){
+
+			$http.get('/api/jobs/' + $routeParams.id )
+				.success(function(response){
+					vm.job = response;
+				})
+				.error(function(err){
+					console.log(err);
+				});
+		}
+		else{
+
+			$http.get('/api/jobs')
+				.success(function(response){
+					vm.jobs = response;
+				});
 		}
 	}
 })();
