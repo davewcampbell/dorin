b9b37646b04f4ff221114dac959d9c55ff6755f3
Dave Campbell <davewcampbell@gmail.com>
2015-01-19 17:47:44 -0500

    moved first run code to an init function. Removed generic submit function, added saveJob and addJob to send put and post calls to api

--- www/lib/app/purge/controllers/purgeController.js
+++ www/lib/app/purge/controllers/purgeController.js
@@ -9,31 +9,57 @@
 	function purgeController($http, $routeParams){
 		var vm = this;
 		vm.title = "Dorin";
-		vm.submit = submit;
+		vm.saveJob = saveJob;
+		vm.addJob = addJob;
 		vm.jobs = [];
 		vm.job = {};
 
 
-		function submit(){
-			console.log("submitted");
-		}
-
-		if($routeParams.id){
-
-			$http.get('/api/jobs/' + $routeParams.id )
+		function saveJob(){
+			var url = '/api/jobs/' + $routeParams.id ;
+			$http.put(url, vm.job)
 				.success(function(response){
-					vm.job = response;
+
 				})
 				.error(function(err){
 					console.log(err);
 				});
 		}
-		else{
 
-			$http.get('/api/jobs')
+		function addJob(){
+			var url = '/api/jobs/';
+
+
+			$http.post(url, vm.job)
 				.success(function(response){
-					vm.jobs = response;
+
+				})
+				.error(function(err){
+					console.log(err);
 				});
 		}
+
+		function init(){
+			if($routeParams.id){
+
+				$http.get('/api/jobs/' + $routeParams.id )
+					.success(function(response){
+						vm.job = response;
+					})
+					.error(function(err){
+						console.log(err);
+					});
+			}
+			else{
+
+				$http.get('/api/jobs')
+					.success(function(response){
+						vm.jobs = response;
+					});
+			}
+		}
+
+		// init
+		init();
 	}
 })();
