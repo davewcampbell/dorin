ba773ee784c9a58ee0bf597c597079893c42396a
Dave Campbell <davewcampbell@gmail.com>
2015-01-27 09:27:07 -0500

    removed  from the controller and made use of the purgeService to to send and receive data from the server

--- www/lib/app/purge/controllers/purgeController.js
+++ www/lib/app/purge/controllers/purgeController.js
@@ -6,33 +6,18 @@
 		.controller("purgeController", purgeController);
 
 
-	function purgeController($http, $routeParams){
+	purgeController.$inject = ['$routeParams', 'purgeService'];
+
+	function purgeController($routeParams, purgeService){
 		var vm = this;
-		vm.title = "Dorin";
+		vm.addJob = addJob;
+		vm.jobs = [];
+		vm.job = {};
 		vm.orderBy = 'name';
 		vm.reverse = false;
 		vm.saveJob = saveJob;
-		vm.addJob = addJob;
 		vm.setOrder = setOrder;
-		vm.jobs = [];
-		vm.job = {};
-
-
-		function saveJob(){
-			var url = '/api/jobs/' + $routeParams.id ;
-			$http.put(url, vm.job)
-				.success(function(response){
-
-				})
-				.error(function(err){
-					console.log(err);
-				});
-		}
-
-		function setOrder(property){
-			vm.orderBy = property;
-			vm.reverse = !vm.reverse;
-		}
+		vm.title = "Dorin";
 
 		function addJob(){
 			var url = '/api/jobs/';
@@ -50,21 +35,37 @@
 		function init(){
 			if($routeParams.id){
 
-				$http.get('/api/jobs/' + $routeParams.id )
-					.success(function(response){
+				purgeService
+					.getJobById($routeParams.id)
+					.then(function(response){
 						vm.job = response;
+						return vm.job;
 					})
-					.error(function(err){
-						console.log(err);
-					});
+
 			}
 			else{
 
-				$http.get('/api/jobs')
-					.success(function(response){
+				purgeService
+					.getAllJobs()
+					.then(function(response){
 						vm.jobs = response;
+						return vm.jobs;
 					});
 			}
+		}
+
+		function saveJob(){
+			purgeService
+				.saveJob(vm.job)
+				.then(function(response){
+					vm.job = response;
+					return response;
+				});
+		}
+
+		function setOrder(property){
+			vm.orderBy = property;
+			vm.reverse = !vm.reverse;
 		}
 
 		// init
