db5b3822da5d49bde90dcc160d90a57b65b0fd05
Dave Campbell <davewcampbell@gmail.com>
2015-01-31 08:24:20 -0500

    injected the toastr module to our main module so we can use it in our app

--- www/lib/app/mainmodule.js
+++ www/lib/app/mainmodule.js
@@ -1,7 +1,7 @@
 (function(){
 	'use strict';
 
-	angular.module("dorin", ['ngRoute'])
+	angular.module("dorin", ['ngRoute', 'toastr'])
 	.config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/jobs', {
