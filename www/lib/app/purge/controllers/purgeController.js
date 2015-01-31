73015af148d32af79f7ebe00f5da811823a8382f
Dave Campbell <davewcampbell@gmail.com>
2015-01-31 08:29:25 -0500

    added a deleteJob function, hooked in toastr for ui alerts

--- www/lib/app/purge/controllers/purgeController.js
+++ www/lib/app/purge/controllers/purgeController.js
@@ -6,11 +6,12 @@
 		.controller("purgeController", purgeController);
 
 
-	purgeController.$inject = ['$routeParams', 'purgeService'];
+	purgeController.$inject = ['$routeParams', '$window', '$location', 'purgeService', 'notifyService'];
 
-	function purgeController($routeParams, purgeService){
+	function purgeController($routeParams, $window, $location, purgeService, notifyService){
 		var vm = this;
 		vm.addJob = addJob;
+		vm.deleteJob = deleteJob;
 		vm.jobs = [];
 		vm.job = {};
 		vm.orderBy = 'name';
@@ -24,8 +25,29 @@
 				.addJob(vm.job)
 				.then(function (response){
 					vm.job = response;
+					vm.jobs.push(vm.job);
+					$location.url('/');
+					notifyService.success("The job was created.", "Success");
 					return vm.job;
-				});
+				})
+				.catch(handleError);
+		}
+
+		function deleteJob(job){
+
+			var result = $window.confirm("Are you sure you wish to delete this job?");
+
+			if(result){
+				purgeService
+					.deleteJob(job._id)
+					.then(function(response){
+						var index = vm.jobs.indexOf(job);
+						vm.jobs.splice(index, 1);
+						notifyService.success("The job was deleted.", "Success");
+						return response;
+					})
+					.catch(handleError);
+			}
 		}
 
 		function init(){
@@ -37,6 +59,7 @@
 						vm.job = response;
 						return vm.job;
 					})
+					.catch(handleError);
 
 			}
 			else{
@@ -46,7 +69,8 @@
 					.then(function(response){
 						vm.jobs = response;
 						return vm.jobs;
-					});
+					})
+					.catch(handleError);
 			}
 		}
 
@@ -55,13 +79,21 @@
 				.saveJob(vm.job)
 				.then(function(response){
 					vm.job = response;
+					$location.url('/');
+					notifyService.success("The job has been saved.", "Success");
 					return response;
-				});
+				})
+				.catch(handleError);
 		}
 
 		function setOrder(property){
 			vm.orderBy = property;
 			vm.reverse = !vm.reverse;
+		}
+
+		function handleError(err){
+			console.log(err);
+			notifyService.error(err, "Oops!");
 		}
 
 		// init
